b574dd202b9316d42437d4ba787b55c99cc45dc5
fles <anotherfles@gmail.com>
2024-09-23 23:49:43 +0300

    Add RepositoryManager

---
+++ HumanResources.Core/Repositories/IRepositoryManager.cs
@@ -0,0 +1,14 @@
+﻿namespace HumanResources.Core.Repositories;
+
+public interface IRepositoryManager
+{
+	ICompanyRepository CompanyRepository { get; }
+	IDepartmentRepository DepartmentRepository { get; }
+	IEmployeeRepository EmployeeRepository { get; }
+	IProfessionRepository ProfessionRepository { get; }
+	ISpecialityRepository SpecialityRepository { get; }
+	IStateRepository StateRepository { get; }
+	IVacancyRepository VacancyRepository { get; }
+	IWorkerRepository WorkerRepository { get; }
+	Task SaveAsync();
+}
---
+++ HumanResources.Infrastructure/Repositories/RepositoryManager.cs
@@ -0,0 +1,35 @@
+﻿using HumanResources.Core.Repositories;
+using HumanResources.Infrastructure.Context;
+
+namespace HumanResources.Infrastructure.Repositories;
+
+public class RepositoryManager : IRepositoryManager
+{
+	private readonly HumanResourcesDbContext _context;
+	private readonly Lazy<ICompanyRepository> _companyRepository;
+	private readonly Lazy<IDepartmentRepository> _departmentRepository;
+	private readonly Lazy<IEmployeeRepository> _employeeRepository;
+	private readonly Lazy<IProfessionRepository> _professionRepository;
+	private readonly Lazy<ISpecialityRepository> _specialityRepository;
+	private readonly Lazy<IStateRepository> _stateRepository;
+	private readonly Lazy<IVacancyRepository> _vacancyRepository;
+	private readonly Lazy<IWorkerRepository> _workerRepository;
+
+    public RepositoryManager(HumanResourcesDbContext context)
+    {
+        _context = context;
+
+
+    }
+    public ICompanyRepository CompanyRepository => throw new NotImplementedException();
+	public IDepartmentRepository DepartmentRepository => throw new NotImplementedException();
+	public IEmployeeRepository EmployeeRepository => throw new NotImplementedException();
+	public IProfessionRepository ProfessionRepository => throw new NotImplementedException();
+	public ISpecialityRepository SpecialityRepository => throw new NotImplementedException();
+	public IStateRepository StateRepository => throw new NotImplementedException();
+	public IVacancyRepository VacancyRepository => throw new NotImplementedException();
+	public IWorkerRepository WorkerRepository => throw new NotImplementedException();
+
+	public async Task SaveAsync() => 
+		await _context.SaveChangesAsync();
+}
