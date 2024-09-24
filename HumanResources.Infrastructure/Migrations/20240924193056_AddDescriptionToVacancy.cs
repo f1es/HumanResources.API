7331339f288d6368e9cdb2f53fa75a839763e07b
Ilya Katushenok <anotherfles@gmail.com>
2024-09-24 23:58:51 +0300

    Add description field to vacancy model

---
+++ HumanResources.Infrastructure/Migrations/20240924193056_AddDescriptionToVacancy.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HumanResources.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToVacancy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Desciption",
+                table: "Vacancies",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Desciption",
+                table: "Vacancies");
+        }
+    }
+}
