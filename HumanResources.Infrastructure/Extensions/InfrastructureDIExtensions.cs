792c3c22d831af9135c5586e7b0f263a2d5b7e51
fles <anotherfles@gmail.com>
2024-09-24 18:09:29 +0300

    DI Extensions

---
+++ HumanResources.Infrastructure/Extensions/InfrastructureDIExtensions.cs
@@ -0,0 +1,13 @@
+﻿using HumanResources.Core.Repositories;
+using HumanResources.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HumanResources.Infrastructure.Extensions;
+
+public static class DIExtensions
+{
+	public static void ConfigureRepositories(this IServiceCollection services)
+	{
+		services.AddScoped<IRepositoryManager, RepositoryManager>();
+	}
+}
---
+++ HumanResources.Usecase/Extensions/UsecaseDIExtensions.cs
@@ -0,0 +1,24 @@
+﻿using HumanResources.Usecase.MapperProfiles;
+using HumanResources.Usecase.Services.Implementations;
+using HumanResources.Usecase.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HumanResources.Usecase.Extensions;
+
+public static class UsecaseDIExtensions
+{
+	public static void ConfigureServices(this IServiceCollection services)
+	{
+		services.AddScoped<ICompanyService, CompanyService>();
+		services.AddScoped<IDepartmentService, DepartmentService>();
+	}
+
+	public static void ConfigureMapperProfiles(this IServiceCollection services)
+	{
+		services.AddAutoMapper(options =>
+		{
+			options.AddProfile<CompanyMapperProfile>();
+			options.AddProfile<DepartmentMapperProfile>();
+		});
+	}
+}
