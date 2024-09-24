e39619d91d3f52d8d722ee74b329c7d380c52a7b
fles <anotherfles@gmail.com>
2024-09-24 18:03:13 +0300

    custom not found exception

---
+++ HumanResources.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace HumanResources.Core.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+        
+    }
+
+    public NotFoundException(string message)
+        : base(message) 
+    {
+        
+    }
+}
