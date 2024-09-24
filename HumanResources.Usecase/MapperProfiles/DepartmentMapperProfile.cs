53698f91768c1b2300e896a91667303cb2b8885b
fles <anotherfles@gmail.com>
2024-09-24 18:05:51 +0300

    Department mapper profile

---
+++ HumanResources.Usecase/MapperProfiles/DepartmentMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HumanResources.Core.Dto.Request;
+using HumanResources.Core.Dto.Response;
+using HumanResources.Core.Models;
+
+namespace HumanResources.Usecase.MapperProfiles;
+public class DepartmentMapperProfile : Profile
+{
+    public DepartmentMapperProfile()
+    {
+        CreateMap<DepartmentRequestDto, Department>();
+
+        CreateMap<Department, DepartmentResponseDto>();
+    }
+}
