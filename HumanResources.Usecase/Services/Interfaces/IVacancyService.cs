48085f250ee98e41798c64c98855e84f8dcf75c8
fles <anotherfles@gmail.com>
2024-09-25 17:10:06 +0300

    Add GetProfessionById to interface

--- HumanResources.Usecase/Services/Interfaces/IVacancyService.cs
+++ HumanResources.Usecase/Services/Interfaces/IVacancyService.cs
@@ -10,4 +10,5 @@ public interface IVacancyService
 	Task<VacancyResponseDto> CreateAsync(Guid companyId, VacancyRequestDto vacancyDto);
 	Task UpdateAsync(Guid companyId, Guid id, VacancyRequestDto vacancyDto);
 	Task DeleteAsync(Guid companyId, Guid id);
+	Task<ProfessionResponseDto> GetProfessionByIdAsync(Guid companyId, Guid id);
 }
