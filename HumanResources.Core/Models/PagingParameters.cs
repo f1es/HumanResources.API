8b2dd94d9452892878aeb61e41934a3d299926e2
fles <anotherfles@gmail.com>
2024-09-26 18:03:26 +0300

    Add paging model and extension

---
+++ HumanResources.Core/Models/PagingParameters.cs
@@ -0,0 +1,5 @@
+﻿namespace HumanResources.Core.Models;
+
+public record PagingParameters(
+	int PageNumber = 1,
+	int PageSize = 10);
---
+++ HumanResources.Infrastructure/Extensions/PagingExtension.cs
@@ -0,0 +1,16 @@
+﻿using HumanResources.Core.Models;
+
+namespace HumanResources.Infrastructure.Extensions;
+
+public static class PagingExtension
+{
+	public static IQueryable<T> Paginate<T>(this IQueryable<T> entities, PagingParameters paging)
+	{
+		if (paging.PageNumber <= 0 || paging.PageSize <= 0)
+			paging = new PagingParameters();
+
+		return entities
+		.Skip((paging.PageNumber - 1) * paging.PageSize)
+		.Take(paging.PageSize);
+	}
+}
