55199babbe7b58b6726e39d0416d714135cf3923
Ilya Katushenok <anotherfles@gmail.com>
2024-09-30 20:58:38 +0300

    Delete employee and speciality model

--- HumanResources.Core/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HumanResources.Core.Models;
-
-public class Employee
-{
-	public Guid Id { get; set; }
-	public string? FirstName { get; set; }
-	public string? LastName { get; set; }
-	public DateTime Birthday { get; set; }
-}
--- HumanResources.Core/Models/Speciality.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HumanResources.Core.Models;
-
-public class Speciality
-{
-	public Guid Id { get; set; }
-	public string? Name { get; set; }
-
-	public ICollection<Worker>? Workers { get; set; }
-}
