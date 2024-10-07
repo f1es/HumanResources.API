bb4cf956804d32d66b21268e9efd4a0c0e0dc68b
Ilya Katushenok <anotherfles@gmail.com>
2024-10-07 20:05:37 +0300

    Add sort extension for companies

--- HumanResources.Infrastructure/Extensions/CompanyRepositoryExtensions.cs
+++ HumanResources.Infrastructure/Extensions/CompanyRepositoryExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using HumanResources.Core.Models;
 using HumanResources.Core.Shared.Parameters;
+using HumanResources.Usecase.Extensions;
+using System.Linq.Dynamic.Core;
 
 namespace HumanResources.Infrastructure.Extensions;
 
@@ -16,5 +18,17 @@ public static class CompanyRepositoryExtensions
 			.ToLower()
 			.Contains(requestParameters.SearchTerm.ToLower()));
 	}
-		
+	
+	public static IQueryable<Company> Sort(this IQueryable<Company> query, CompanyRequestParameters requestParameters)
+	{
+		if (string.IsNullOrWhiteSpace(requestParameters.OrederByQuery))
+			return query.OrderBy(c => c.Name);
+
+		var sortQuery = SortQueryBuilder.BuildSortQuery<Company>(requestParameters.OrederByQuery);
+
+		if (string.IsNullOrWhiteSpace(sortQuery))
+			return query.OrderBy(c => c.Name);
+
+		return query.SortByDynamicQuery(sortQuery);
+	}
 }
