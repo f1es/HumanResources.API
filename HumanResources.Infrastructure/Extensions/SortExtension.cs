d506723694c7999257364048bba128d21ed1f601
Ilya Katushenok <anotherfles@gmail.com>
2024-10-07 19:55:58 +0300

    Add dynamic orderBy extension

---
+++ HumanResources.Infrastructure/Extensions/SortExtension.cs
@@ -0,0 +1,13 @@
+﻿using HumanResources.Usecase.Extensions;
+using System.Linq.Dynamic.Core;
+
+namespace HumanResources.Infrastructure.Extensions;
+
+public static class SortExtension
+{
+	public static IQueryable<T> SortByDynamicQuery<T>(this IQueryable<T> values, string sortQuery) //Prop asc, Prop desc
+	{
+		var query = SortQueryBuilder.BuildSortQuery<T>(sortQuery);
+		return values.OrderBy(query);
+	}
+}
