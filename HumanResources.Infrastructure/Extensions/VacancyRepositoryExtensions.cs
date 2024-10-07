d22cc4666fa66cd6b06d9ee0547d92c4e6cd32c4
Ilya Katushenok <anotherfles@gmail.com>
2024-10-07 20:09:29 +0300

    Add sort extension for vacancies

--- HumanResources.Infrastructure/Extensions/VacancyRepositoryExtensions.cs
+++ HumanResources.Infrastructure/Extensions/VacancyRepositoryExtensions.cs
@@ -1,6 +1,21 @@
-﻿namespace HumanResources.Infrastructure.Extensions;
+﻿using HumanResources.Core.Models;
+using HumanResources.Core.Shared.Parameters;
+using HumanResources.Usecase.Extensions;
+
+namespace HumanResources.Infrastructure.Extensions;
 
 public static class VacancyRepositoryExtensions
 {
-	
+	public static IQueryable<Vacancy> Sort(this IQueryable<Vacancy> query, CompanyRequestParameters requestParameters)
+	{
+		if (string.IsNullOrWhiteSpace(requestParameters.OrederByQuery))
+			return query.OrderBy(v => v.ReceiptDate);
+
+		var sortQuery = SortQueryBuilder.BuildSortQuery<Company>(requestParameters.OrederByQuery);
+
+		if (string.IsNullOrWhiteSpace(sortQuery))
+			return query.OrderBy(v => v.ReceiptDate);
+
+		return query.SortByDynamicQuery(sortQuery);
+	}
 }
