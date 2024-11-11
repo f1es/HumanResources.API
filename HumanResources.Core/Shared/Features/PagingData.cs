75f63467f0dde6781de80df03e754c525c6871ba
fles <anotherfles@gmail.com>
2024-11-11 19:48:18 +0300

    Add PagedList class, which contains paging data

---
+++ HumanResources.Core/Shared/Features/PagingData.cs
@@ -0,0 +1,12 @@
+﻿namespace HumanResources.Core.Shared.Features;
+
+public class PagingData
+{
+	public int CurrentPage { get; set; }
+	public int PageCount { get; set; }
+	public int PageSize { get; set; }
+	public int Count { get; set; }
+
+	public bool HasNext => CurrentPage > 1;
+	public bool HasPrevious => CurrentPage < PageCount;
+}
