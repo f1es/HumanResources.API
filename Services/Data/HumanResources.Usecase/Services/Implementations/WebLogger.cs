2d6300d62307fbbc46db74b1db35fb12f7ead129
Ilya Katushenok <anotherfles@gmail.com>
2024-11-19 19:41:24 +0300

    Add methods for warn, info, debug, error

--- Services/Data/HumanResources.Usecase/Services/Implementations/WebLogger.cs
+++ Services/Data/HumanResources.Usecase/Services/Implementations/WebLogger.cs
@@ -7,6 +7,7 @@ namespace HumanResources.Usecase.Services.Implementations;
 public class WebLogger : IWebLogger
 {
 	private WebSocket _webSocket;
+	private ILoggerManager _loggerManager;
 	public async Task LogAsync(string message)
 	{
 		if (_webSocket is null)
@@ -20,8 +21,39 @@ public class WebLogger : IWebLogger
 			await _webSocket.SendAsync(new ArraySegment<byte>(logBytes, 0, logBytes.Length), WebSocketMessageType.Text, true, CancellationToken.None); 
 		}
 	}
+
+	public async Task LogDebugAsync(string message)
+	{
+		await LogAsync(message);
+		_loggerManager.LogDebug(message);
+
+	}
+
+	public async Task LogErrorAsync(string message)
+	{
+		await LogAsync(message);
+		_loggerManager.LogError(message);
+	}
+
+	public async Task LogInfoAsync(string message)
+	{
+		await LogAsync(message);
+		_loggerManager.LogInfo(message);
+	}
+
+	public async Task LogWarnAsync(string message)
+	{
+		await LogAsync(message);
+		_loggerManager.LogWarn(message);
+	}
+
 	public void UseSocket(WebSocket webSocket)
 	{
 		_webSocket = webSocket;
 	}
+
+    public WebLogger(ILoggerManager loggerManager)
+    {
+        _loggerManager = loggerManager;
+    }
 }
--- Services/Data/HumanResources.Usecase/Services/Interfaces/IWebLogger.cs
+++ Services/Data/HumanResources.Usecase/Services/Interfaces/IWebLogger.cs
@@ -5,5 +5,9 @@ namespace HumanResources.Usecase.Services.Interfaces;
 public interface IWebLogger
 {
 	Task LogAsync(string message);
+	Task LogInfoAsync(string message);
+	Task LogWarnAsync(string message);
+	Task LogDebugAsync(string message);
+	Task LogErrorAsync(string message);
 	void UseSocket(WebSocket webSocket);
 }
