83d03a38603b6ed08a747f8be2a536046e3b87e5
fles <anotherfles@gmail.com>
2024-11-20 12:32:51 +0300

    Add claims reader class

---
+++ Services/Data/HumanResources.Usecase/Extensions/ClaimsReader.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace HumanResources.Usecase.Extensions;
+
+public static class ClaimsReader
+{
+	public static string GetUsername(this IEnumerable<Claim> claims)
+	{
+		var usernameClaim = claims.FirstOrDefault(c => c.Type == "name");
+		if (usernameClaim is null)
+		{
+			return "none";
+		}
+		else
+		{
+			return usernameClaim.Value;
+		}
+	}
+}
