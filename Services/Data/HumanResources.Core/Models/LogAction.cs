f38b9c02cb03f7101f65600f046626be2a7096e9
fles <anotherfles@gmail.com>
2024-11-20 12:31:47 +0300

    Add log action model

---
+++ Services/Data/HumanResources.Core/Models/LogAction.cs
@@ -0,0 +1,19 @@
+﻿namespace HumanResources.Core.Models;
+
+public class LogAction
+{
+	public string Username { get; set; }
+	public string LogType { get; set; }
+	public string Message { get; set; }
+	public int StatusCode { get; set; }
+	public DateTime Date { get; set; }
+
+    public LogAction(string username, string message, string logType, int satatusCode)
+    {
+        Username = username;
+		LogType = logType;
+		Message = message;
+		StatusCode = satatusCode;
+		Date = DateTime.Now;
+    }
+}
